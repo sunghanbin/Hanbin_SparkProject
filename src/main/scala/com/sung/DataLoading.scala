49c6b1f5c888850ddaeeb950eb9934f19a53bee7
SMART-15 <gksqlsekdna@gmail.com>
2018-06-15 20:16:25 +0900

    version 1.00

--- src/main/scala/com/sung/DataLoading.scala
+++ src/main/scala/com/sung/DataLoading.scala
@@ -26,14 +26,14 @@ object DataLoading {
 
    // 절대 경로 //하드코딩??
 
-    var TestData2 = spark.read.format("csv").option("header","true").load("c:/spark/bin/data/"+File)
-
+    var TestData2 = spark.read.format("csv").option("header","true").option("Delimiter",";").load("c:/spark/bin/data/"+File)
+                                                                     // ; 로 나눈다
     // 메모리 테이블 생성
 
     TestData2.createOrReplaceTempView("TestTable")
 
     // 메모리 테이블 볼때 는 프린트 문 사용
-    println(TestData2)
+    println(TestData2.show)
 
 
 
@@ -66,7 +66,117 @@ object DataLoading {
 //      +--------+------------+--------+-------+
 
 
+
+    // option("Delimiter",";") 를 사용했을 때
+//      +----------------+--------------------+
+//      |      PARAM_NAME|         PARAM_VALUE|
+//      +----------------+--------------------+
+//      |productgroupList|REF,TV,MOBILE,WM,APS|
+//      |         myorder|                  17|
+//      |          myIter|                   3|
+//      | resultTableName|       SEASON_RESULT|
+//      |       ap2idList|300114,300122,300130|
+//      |           jobid|             batch_s|
+//      |          prefix|                 brt|
+//      +----------------+--------------------+
+
+
+
+
+
+    //    기존 데이터를 Delimiter 옵션을 사용하지 않고 가져왔을때
+//      +----------------------+
+//      |PARAM_NAME;PARAM_VALUE|
+//      +----------------------+
+//      |  productgroupList;REF|
+//      |            myorder;17|
+//      |              myIter;3|
+//      |  resultTableName;S...|
+//      |      ap2idList;300114|
+//      |         jobid;batch_s|
+//      |            prefix;brt|
+//      +----------------------+
     ///////////////////////////////////////////////////////
+
+
+
+///////////////// DB에 접속하여 데이터 가져오기 ////////////////////////
+
+//    Postgres/greenplumDB //
+
+    //접속정보 설정
+    var staticUrl = "jdbc:postgresql://192.168.110.111:5432/kopo"
+    var staticUser = "kopo"
+    var staticPw = "kopo"
+    var selloutData = "kopo_channel_seasonality"
+
+    //jdbc(java database connectivity)연결 자바에서 데이터 베이스를 연결해주는 라이브러리
+    var selloutDataFromPg = spark.read.format("jdbc").
+      options(Map("url"->staticUrl,"dbtable"->selloutData,"user"->staticUser,"password"->staticPw)).load
+
+    //메모리 테이블 생성
+    selloutDataFromPg.createOrReplaceTempView("selloutTable")
+
+    selloutDataFromPg.show(1)
+
+    ////////////////////////////
+
+    /// Mysql //
+                        //:접속db이름:ip주소//포트번호/테이블스페이스명??
+    var staticUrl = "jdbc:mysql://192.168.110.112:3306/kopo"
+    var staticUser = "root"// 계정
+    var staticPw = "P@ssw0rd"// 비번
+    var selloutDb = "KOPO_PRODUCT_VOLUME"// db 에서 가져올 데이터 이름
+
+    //이하 동일
+
+
+    // Oracle //
+
+    var staticUrl = "jdbc:oracle:thin:@192.168.110.16:1522/XE"
+    var staticUser = "haiteam"
+    var staticPw = "haiteam"
+    var selloutDb = "kopo_product_volume"
+
+    // 이하 동일
+
+
+    // Sqlserver(Microsoft SQL) //
+    var staticUrl = "jdbc:sqlserver://192.168.110.70:1433;databaseName=kopo"//하나 다름 databaseName=kopo
+    var staticUser = "haiteam"
+    var staticPw = "haiteam"
+    var selloutDb = "KOPO_PRODUCT_VOLUME"
+
+
+    ///////////////////////////////////////////////////////////////
+
+
+
+
+    ////////////// DB 에 저장 하기 ///////////////////////
+
+    //    db 에 접속 할 때 처럼 접속정보 설정
+    var outputUrl = "jdbc:oracle:thin:@127.0.0.1:1521/XE"
+    var outputUser = "sung_2"
+    var outputPw = "sung_2"
+
+    // 저장
+    var prop = new java.util.Properties
+    prop.setProperty("driver", "oracle.jdbc.OracleDriver")
+    prop.setProperty("user", outputUser)
+    prop.setProperty("password",outputPw)
+    var table = "kopo_batch_season_mpara"
+    //append
+    selloutDataFromPg.write.mode("overwrite").jdbc(outputUrl, table, prop)
+
+
+
+
+
+
+
+
+
 
 
   }
